1fa028d02709b1151ce06356e850c0df8ce85c1c
jrthompson33 <jrthompson@gatech.edu>
2014-11-20 01:22:55 -0500

    Formatted the index.html file. Try to use 4 spaces for your tabs in your text editor instead of 2.
    Fixing the issue with the "node" class conflict between the phylo and radial vis's.
    
    Fixed to allow for updating of radial.
    
    Fixed the gradients for the radial.

--- js/radial.js
+++ js/radial.js
@@ -9,20 +9,19 @@ var geneMap = {
 
 var colorMap = {
     "blacktipreefshark": "#DD1C77",
-    "bluewhale": "#3182BD",
     "giantgrouper": "#C994C7",
     "gianttrevally": "#756BB1",
-    "greatwhiteshark": "#636363",
-    "hammerhead": "#99D8C9",
+    "harborseal": "#636363",
+    "wobblegong": "#99D8C9",
     "human": "#FC9272",
-    "sandbarshark": "#FC9272",
-    "swordfish": "#9EBCDA",
-    "tigershark": "#FEC44F",
+    "sandbarshark": "#9EBCDA",
+    "saltwatercrocodile": "#FEC44F",
     "whaleshark": "#1C9099",
     "zebrafish": "#31A354"
-}
+};
 
 var species = [];
+var classMap = {};
 var geneLinks = [];
 
 // Values needed for the layout of the radial node-linked graph
@@ -53,15 +52,13 @@ var svgDefs = radialSvg.append("svg:defs");
 // Define the line type that will be used to connect genes
 var line = d3.svg.line.radial()
     .interpolate("bundle")
-    .tension(.60)
+    .tension(0.55)
     .radius(function(d) { return d.y; })
     .angle(function(d) { return d.x / 180 * Math.PI; });
 
 // Max values to set so I can adjust the radial view
 var maxGeneLength = 0,
-    gradientCounter = 0,
-    linkEndCounter = 0,
-    linkStartCounter = 0;
+    gradientCounter = 0;
 
 // On document ready, process the json file
 $(document).ready(function () {
@@ -78,6 +75,7 @@ function processGenomeData(data) {
 
     // Go through each species and make a link within each gene to the species that have that same gene
     species.forEach(function(s,i,species){
+        classMap[s.name.toLowerCase()] = s;
         s.className = s.name.toLowerCase();
         s.children = [];
         s.genes.forEach(function(g,ii, genes){
@@ -111,15 +109,45 @@ function processGenomeData(data) {
         });
     });
 
-    var selectSpecies = [species[2],species[4], species[7],  species[1]];
-    drawGeneChart(selectSpecies);
+    drawGeneChart();
 
 }
 
-function drawGeneChart(selectedSpecies){
+function drawGeneChart(){
+
+    geneMap = {
+        "name" : "",
+        "children" : []
+    };
+
+    geneBundle = d3.layout.bundle();
+    gradientCounter = 0;
+
+    radialSvg.selectAll("*").remove();
+    svgDefs = radialSvg.append("svg:defs");
+
+    geneCluster = d3.layout.cluster()
+        .size([360, innerRadius])
+        .sort(function(a, b) { return d3.descending(a.position, b.position);})
+        .value(function(d) { return d.length; });
+
+    var selectedSpecies = [];
+
+    for(var o = 1; o < 5; o++){
+        var select = $("#organism"+o)[0];
+        var c = select.options[select.selectedIndex].value;
+        if(c != "undefined"){
+            selectedSpecies.push(classMap[c]);
+        }
+    }
+
+    if(selectedSpecies.length < 2){
+        return;
+    }
+
     var mergedLinks = [];
 
-    for(var s = 0; s < 4; s++){
+    for(var s = 0; s < selectedSpecies.length; s++){
         geneMap.children.push(selectedSpecies[s]);
     }
 
@@ -127,17 +155,65 @@ function drawGeneChart(selectedSpecies){
         .domain([0,maxGeneLength])
         .range([0,50]);
 
-    var nodes = geneCluster.nodes(geneMap)
+    var geneStrokeScale = d3.scale.linear()
+        .domain([0,maxGeneLength])
+        .range([0,5]);
 
-    radialSvg.selectAll(".node-dot")
+    var nodes = geneCluster.nodes(geneMap);
+
+    // Looks into all of the selected species - and finds the ones that are now nodes
+    // Only want links from the first to the next, then from the second to the third, then from the third to fourth
+    for(var s = 0; s < selectedSpecies.length; s++){
+        var other = [];
+        if(selectedSpecies.length == 2){
+            other = s == 0 ? [1] : [0];
+        }
+        if(selectedSpecies.length == 3){
+            other = s == 0 ? [1,2] : (s == 1 ? [0,2] : [0,1]);
+        }
+        if(selectedSpecies.length == 4){
+            other = s == 0 ? [1,2,3] : (s == 1 ? [0,2,3] : (s == 2 ? [0,1,3] : [0,1,2]));
+        }
+        for(var l = 0; l < selectedSpecies[s].links.length; l++){
+            // For this link make sure the target is a selected species
+            other.forEach(function(o){
+                var parent = selectedSpecies[s].links[l].target["parent"]
+                if(parent && parent == selectedSpecies[o]) {
+                    mergedLinks.push(selectedSpecies[s].links[l]);
+                }
+            })
+        }
+    }
+
+    radialSvg.selectAll(".links")
+        .data(geneBundle(mergedLinks))
+        .enter().append("path")
+        .attr("class", function(d){
+            var linkClass = 'links link-' + d[0]['className'] + ' link-' + d[4]['className'];
+            return linkClass
+        })
+        .attr("id", function(d){
+            return 'link-' + d[0]['className'] + '-' + d[4]['className']
+        })
+        .attr("d", line)
+        .style("stroke-width", function(d){
+            return geneStrokeScale(d[0].length);
+        })
+        .style("stroke", function(d){
+            return 'url(#' + getGeneGradient(d[0]["x"] - 90, d[4]["x"] - 90, d[0]["speciesClass"], d[4]["speciesClass"]) +')'
+        });
+
+    radialSvg.selectAll(".radnode-dot")
         .data(nodes.filter(function(n) { return n.depth == 2; }))
         .enter().append("g")
         .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
         .append("rect")
         .attr("class", function(d){
-            return 'node-dot ' + 'nodedot-' + d.className
+            return 'radnode-dot ' + 'nodedot-' + d.className
         })
-        .attr('y', -5)
+        .attr('y', -6)
+        .attr('x', -2)
+        .attr("angle", function(d) { return d.x - 90; })
         .attr('height', 12)
         .attr('width', function(d){
             return geneBarScale(d.length);
@@ -149,10 +225,10 @@ function drawGeneChart(selectedSpecies){
         .on("mouseout", hideGeneConnections)
         .on("click", showGeneComparison);
 
-    radialSvg.selectAll(".node")
+    radialSvg.selectAll(".radnode")
         .data(nodes.filter(function(n) { return n.depth == 2; }))
         .enter().append("g")
-        .attr("class", 'node')
+        .attr("class", 'radnode')
         .attr("transform", function(d) {
             var translatevalue = d.y + 5;
             translatevalue += geneBarScale(d.length);
@@ -161,6 +237,7 @@ function drawGeneChart(selectedSpecies){
         .append("text")
         .attr("dx", function(d) { return d.x < 180 ? 0 : 0; })
         .attr("dy", "5")
+        .attr("angle", function(d) { return d.x - 90; })
         .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
         .attr("transform", function(d) { return d.x < 180 ? null : "rotate(180)"; })
         .text(function(d) { return d.name; })
@@ -175,37 +252,8 @@ function drawGeneChart(selectedSpecies){
         .on("mouseout", hideGeneConnections)
         .on("click", showGeneComparison);
 
-    //$(".node").mousemove(setGenePopupPosition);
-    //$(".node-dot").mousemove(setGenePopupPosition);
-
-    // Looks into all of the selected species - and finds the ones that are now nodes
-    // TODO this needs to be changed to only have targets that are in the selectedSpecies array
-    // Only want links from the first to the next, then from the second to the third, then from the third to fourth
-    for(var s = 0; s < 4; s++){
-        geneMap.children.push(selectedSpecies[s]);
-        for(var l = 0; l < selectedSpecies[s].links.length; l++){
-            if(selectedSpecies[s].links[l].target["parent"]) {
-                mergedLinks.push(selectedSpecies[s].links[l]);
-            }
-        }
-    }
-
-
-
-    radialSvg.selectAll(".links")
-        .data(geneBundle(mergedLinks))
-        .enter().append("path")
-        .attr("class", function(d){
-            var linkClass = 'links link-' + d[0]['className'] + ' link-' + d[4]['className'];
-            return linkClass
-        })
-        .attr("id", function(d){
-            return 'link-' + d[0]['className'] + '-' + d[4]['className']
-        })
-        .attr("d", line)
-        .style("stroke", function(d){
-            return 'url(#' + getGeneGradient(d[0]["length"], d[4]["length"], d[0]["speciesClass"], d[4]["speciesClass"]) +')'
-        });
+    $(".radnode").mousemove(setGenePopupPosition);
+    $(".radnode-dot").mousemove(setGenePopupPosition);
 }
 
 
@@ -238,7 +286,7 @@ function showGeneConnections(d) {
         .classed('highlight', true)
         .classed('circle-text-dim', false);
 
-    radialSvg.selectAll('.node-dot')
+    radialSvg.selectAll('.radnode-dot')
         .style("opacity", .05);
 
     radialSvg.selectAll('path.links')
@@ -259,41 +307,16 @@ function showGeneConnections(d) {
             .style("opacity", 1)
     });
 
-    //$("#node-info").empty()
-    //
-    //if(d.nodeType == 'game'){
-    //    $("#gameTemplate").tmpl( {
-    //        name: d.name,
-    //        sales: roundSales(d.size),
-    //        rating: getRating(d.gameRating),
-    //        color: getColor(d.nodeType, d.size),
-    //        weaponCount: d.weapons.length,
-    //        topicCount: d.topics.length
-    //    }).appendTo( "#node-info" );
-    //
-    //    var weapons = (d.weapons.length > 0)? d.weapons: ['none'];
-    //    $.each(weapons, function(i, w){
-    //        $("#listTemplate").tmpl( {item: w}).appendTo( "#node-weapon-references .node-data" );
-    //    })
-    //
-    //    var topics = (d.topics.length > 0)? d.topics: ['none'];
-    //    $.each(topics, function(i, t){
-    //        $("#listTemplate").tmpl( {item: t}).appendTo( "#node-topic-references .node-data" );
-    //    })
-    //} else if(d.nodeType == 'weapon' ){
-    //    $("#weaponTemplate").tmpl( {
-    //        name: (d.name == 'axe') ? 'an ' + d.name: 'a ' + d.name,
-    //        color: getColor(d.nodeType, d.size),
-    //        count: addCommas(d.numGames)
-    //    }).appendTo( "#node-info" );
-    //} else if( d.nodeType == 'topic'){
-    //    $("#weaponTopicTemplate").tmpl( {
-    //        name: (d.name.toLowerCase().search('use') >= 0)? 'the ' + d.name.toLowerCase() : d.name.toLowerCase(),
-    //        color: getColor(d.nodeType, d.size),
-    //        count: (d.numGames > 1) ? addCommas(d.numGames)	+ ' games have': addCommas(d.numGames)	+ ' game has'
-    //    }).appendTo( "#node-info" );
-    //}
-    //$("#node-info").show()
+    $("#node-info").empty()
+
+    $("#geneTemplate").tmpl( {
+        speciesName: d.speciesClass,
+        length: d.length,
+        geneName: d.name,
+        color: getGeneColor(d.speciesClass, d.length)
+    }).appendTo( "#node-info" );
+
+    $("#node-info").show()
 
 }
 
@@ -309,7 +332,8 @@ function showGeneComparison(d) {
 }
 
 function hideGeneConnections(d) {
-    //$("#node-info").hide();
+    $("#node-info").hide();
+
     radialSvg.selectAll('path.links')
         .style("opacity", 1);
 
@@ -317,26 +341,67 @@ function hideGeneConnections(d) {
         .classed('circle-text-dim', false)
         .classed('highlight', false);
 
-    radialSvg.selectAll('.node-dot')
+    radialSvg.selectAll('.radnode-dot')
         .style("opacity", 1);
 }
 
 function getGeneGradient(startValue, endValue, species1, species2){
 
+    // Create a gradient id so it can be referenced by the path-style-stroke
     var gradientId = "gradient" + gradientCounter;
 
-    var gradient = svgDefs.append("svg:linearGradient")
-        .attr("id", gradientId);
+    // Used to create the angle for the gradient
+    var x1 = Math.cos(startValue * Math.PI / 180) / 2 + 0.5,
+        y1 = Math.sin(startValue * Math.PI / 180) / 2 + 0.5,
+        x2 = Math.cos(endValue * Math.PI / 180) / 2 + 0.5,
+        y2 = Math.sin(endValue * Math.PI / 180) / 2 + 0.5;
 
+    // Set the x-y coordinates that represent the angle of this linear gradient
+    var gradient = svgDefs.append("svg:linearGradient")
+        .attr("id", gradientId)
+        .attr("x1", x1).attr("y1", y1)
+        .attr("x2", x2).attr("y2", y2);
+
+    // Define a stop color - the first one is the source node
     gradient.append("svg:stop")
         .attr("offset", "10%")
         .attr("stop-color", getGeneColor(species1, startValue));
 
+    // Define a stop color - the second one is the target node
     gradient.append("svg:stop")
         .attr("offset", "90%")
         .attr("stop-color", getGeneColor(species2, endValue));
 
+    // Increment the counter so we have unique names
     gradientCounter++;
+
+    // Return the id for the path to reference
     return gradientId;
+}
+
+function setGenePopupPosition(e){
+    e = jQuery.event.fix(e);
+
+    var a = e.target.attributes["angle"].value;
+
+    var a = a * Math.PI / 180;
+
+    // Center position of the radial vis
+    var cY = $("#radialVis").offset().top + radius - 50;
+    var cX = $("#radialVis").offset().left + radius + 75;
+
+    // Subtract midpoints, so that midpoint is translated to origin
+    // and add it in the end again
+    var xr = (radius - 100) * Math.cos(a) + cX,
+        yr = (radius - 100) * Math.sin(a) + cY;
+
+    var nTop    = yr - $("#node-info").outerHeight()/2;
+    var nLeft   = xr - $("#node-info").outerWidth()/2;
+
+    $('.radial-popup').css({
+        top: nTop,
+        left: nLeft
+    });
+
 }
 
